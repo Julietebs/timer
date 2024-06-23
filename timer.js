d91ac895ddc336dd3feda7c78809e05d7f8a71cd
Julietebs <juliete.jbs@gmail.com>
2024-06-23 15:29:51 -0300

    Initial commit

---
+++ timer.js
@@ -0,0 +1,38 @@
+function criaSeg(segundos){
+    const data = new Date(segundos*1000)
+    return data.toLocaleTimeString('pt-BR', {
+        hour12:false,
+        timeZone: 'GMT'
+    })
+}
+
+const hora = document.querySelector('#hora')
+const iniciar = document.querySelector('#iniciar')
+const pausar = document.querySelector('#pausar')
+const zerar = document.querySelector('#zerar')
+let segundos = 0
+let timer;
+
+iniciar.addEventListener('click', inicio);
+pausar.addEventListener('click', pausa)
+zerar.addEventListener('click', zerou)
+
+
+function inicio(){
+    timer = setInterval(function(){
+        segundos++
+        hora.innerHTML = criaSeg(segundos)
+    },1000)
+}
+
+function pausa(){
+    clearInterval(timer)
+    
+}
+
+function zerou(){
+    clearInterval(timer)
+    hora.innerHTML = '00:00:00'
+    segundos = 0    
+}
+
